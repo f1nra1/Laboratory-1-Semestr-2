41a6ed93aedaca811a6dd1906127dfd4485efe7c
f1nra1 <sportarsenin83@gmail.com>
2025-10-28 11:04:02 +0700

    c++

--- tree.cpp
+++ tree.cpp
@@ -5,35 +5,43 @@
 using namespace std;
 
 int getHeight(TNode* node) {
-    return node ? node->height : 0;
+    if (node != nullptr) {
+        return node -> height;
+    } else {
+        return 0;
+    }
 }
 
 void updateHeight(TNode* node) {
     if (node) {
-        node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+        node -> height = 1 + max(getHeight(node -> left), getHeight(node -> right));
     }
 }
 
 int getBalance(TNode* node) {
-    return node ? getHeight(node->left) - getHeight(node->right) : 0;
+    if (node != nullptr) {
+        return getHeight(node -> left) - getHeight(node -> right);
+    } else {
+        return 0;
+    }
 }
 
 TNode* createNode(string data) {
     TNode* node = new TNode;
-    node->data = data;
-    node->left = nullptr;
-    node->right = nullptr;
-    node->height = 1;
+    node -> data = data;
+    node -> left = nullptr;
+    node -> right = nullptr;
+    node -> height = 1;
     return node;
 }
 
 // Правый поворот
 TNode* rotateRight(TNode* y) {
-    TNode* x = y->left;
-    TNode* T2 = x->right;
+    TNode* x = y -> left;
+    TNode* T2 = x -> right;
 
-    x->right = y;
-    y->left = T2;
+    x -> right = y;
+    y -> left = T2;
 
     updateHeight(y);
     updateHeight(x);
@@ -43,11 +51,11 @@ TNode* rotateRight(TNode* y) {
 
 // Левый поворот
 TNode* rotateLeft(TNode* x) {
-    TNode* y = x->right;
-    TNode* T2 = y->left;
+    TNode* y = x -> right;
+    TNode* T2 = y -> left;
 
-    y->left = x;
-    x->right = T2;
+    y -> left = x;
+    x -> right = T2;
 
     updateHeight(x);
     updateHeight(y);
@@ -63,24 +71,24 @@ TNode* balance(TNode* node) {
     int bal = getBalance(node);
 
     // Left-Left
-    if (bal > 1 && getBalance(node->left) >= 0) {
+    if (bal > 1 && getBalance(node -> left) >= 0) {
         return rotateRight(node);
     }
 
     // Left-Right
-    if (bal > 1 && getBalance(node->left) < 0) {
-        node->left = rotateLeft(node->left);
+    if (bal > 1 && getBalance(node -> left) < 0) {
+        node -> left = rotateLeft(node -> left);
         return rotateRight(node);
     }
 
     // Right-Right
-    if (bal < -1 && getBalance(node->right) <= 0) {
+    if (bal < -1 && getBalance(node -> right) <= 0) {
         return rotateLeft(node);
     }
 
     // Right-Left
-    if (bal < -1 && getBalance(node->right) > 0) {
-        node->right = rotateRight(node->right);
+    if (bal < -1 && getBalance(node -> right) > 0) {
+        node -> right = rotateRight(node -> right);
         return rotateLeft(node);
     }
 
@@ -94,10 +102,10 @@ TNode* insertNode(TNode* node, string data, bool& inserted) {
         return createNode(data);
     }
 
-    if (data < node->data) {
-        node->left = insertNode(node->left, data, inserted);
-    } else if (data > node->data) {
-        node->right = insertNode(node->right, data, inserted);
+    if (data < node -> data) {
+        node -> left = insertNode(node -> left, data, inserted);
+    } else if (data > node -> data) {
+        node -> right = insertNode(node -> right, data, inserted);
     } else {
         // Дубликат - не вставляем
         return node;
@@ -108,8 +116,8 @@ TNode* insertNode(TNode* node, string data, bool& inserted) {
 
 // Найти минимальный узел
 TNode* findMin(TNode* node) {
-    while (node && node->left) {
-        node = node->left;
+    while (node && node -> left) {
+        node = node -> left;
     }
     return node;
 }
@@ -118,24 +126,24 @@ TNode* findMin(TNode* node) {
 TNode* deleteNode(TNode* node, string data, bool& deleted) {
     if (!node) return nullptr;
 
-    if (data < node->data) {
-        node->left = deleteNode(node->left, data, deleted);
-    } else if (data > node->data) {
-        node->right = deleteNode(node->right, data, deleted);
+    if (data < node -> data) {
+        node -> left = deleteNode(node -> left, data, deleted);
+    } else if (data > node -> data) {
+        node -> right = deleteNode(node -> right, data, deleted);
     } else {
         deleted = true;
 
         // Узел с одним ребенком или без детей
-        if (!node->left || !node->right) {
-            TNode* temp = node->left ? node->left : node->right;
+        if (!node -> left || !node -> right) {
+            TNode* temp = node -> left ? node -> left : node -> right;
             delete node;
             return temp;
         }
 
         // Узел с двумя детьми
-        TNode* temp = findMin(node->right);
-        node->data = temp->data;
-        node->right = deleteNode(node->right, temp->data, deleted);
+        TNode* temp = findMin(node -> right);
+        node -> data = temp -> data;
+        node -> right = deleteNode(node -> right, temp -> data, deleted);
         deleted = true;
     }
 
@@ -144,35 +152,35 @@ TNode* deleteNode(TNode* node, string data, bool& deleted) {
 
 // Публичные функции
 void CreateTree(BinaryTree* tree) {
-    tree->root = nullptr;
-    tree->size = 0;
+    tree -> root = nullptr;
+    tree -> size = 0;
 }
 
 void TPush(BinaryTree* tree, string data) {
     bool inserted = false;
-    tree->root = insertNode(tree->root, data, inserted);
+    tree -> root = insertNode(tree -> root, data, inserted);
     if (inserted) {
-        tree->size++;
+        tree -> size++;
     }
 }
 
 void TDelete(BinaryTree* tree, string data) {
     bool deleted = false;
-    tree->root = deleteNode(tree->root, data, deleted);
+    tree -> root = deleteNode(tree -> root, data, deleted);
     if (deleted) {
-        tree->size--;
+        tree -> size--;
     }
 }
 
 bool TSearch(BinaryTree* tree, string data) {
-    TNode* current = tree->root;
+    TNode* current = tree -> root;
     while (current) {
-        if (data == current->data) {
+        if (data == current -> data) {
             return true;
-        } else if (data < current->data) {
-            current = current->left;
+        } else if (data < current -> data) {
+            current = current -> left;
         } else {
-            current = current->right;
+            current = current -> right;
         }
     }
     return false;
@@ -199,15 +207,15 @@ void PrintTree(TNode* root) {
         string ptrStr = QPop(&q);
         TNode* current = (TNode*)(stoll(ptrStr));
 
-        cout << current->data;
+        cout << current -> data;
         nodesInLevel--;
 
-        if (current->left) {
-            QPush(&q, to_string((long long)current->left));
+        if (current -> left) {
+            QPush(&q, to_string((long long)current -> left));
             nodesInNext++;
         }
-        if (current->right) {
-            QPush(&q, to_string((long long)current->right));
+        if (current -> right) {
+            QPush(&q, to_string((long long)current -> right));
             nodesInNext++;
         }
 
@@ -230,31 +238,31 @@ void PrintTree(TNode* root) {
 // Прямой обход (Pre-order: корень -> левый -> правый)
 void preOrder(TNode* node) {
     if (!node) return;
-    cout << node->data << " ";
-    preOrder(node->left);
-    preOrder(node->right);
+    cout << node -> data << " ";
+    preOrder(node -> left);
+    preOrder(node -> right);
 }
 
 // Симметричный обход (In-order: левый -> корень -> правый)
 void inOrder(TNode* node) {
     if (!node) return;
-    inOrder(node->left);
-    cout << node->data << " ";
-    inOrder(node->right);
+    inOrder(node -> left);
+    cout << node -> data << " ";
+    inOrder(node -> right);
 }
 
 // Обратный обход (Post-order: левый -> правый -> корень)
 void postOrder(TNode* node) {
     if (!node) return;
-    postOrder(node->left);
-    postOrder(node->right);
-    cout << node->data << " ";
+    postOrder(node -> left);
+    postOrder(node -> right);
+    cout << node -> data << " ";
 }
 
 // Уничтожение дерева
 void DestroyTree(TNode* node) {
     if (!node) return;
-    DestroyTree(node->left);
-    DestroyTree(node->right);
+    DestroyTree(node -> left);
+    DestroyTree(node -> right);
     delete node;
 }
